200ad7e454e91e57821fb59aa5fddfeb99021ca2
TheAIBot <andreasuranus@hotmail.com>
2022-12-30 22:22:48 +0100

    Generalize unrolled loop

--- Equation.Solver/NandOperator.cs
+++ Equation.Solver/NandOperator.cs
@@ -1,4 +1,8 @@
-﻿namespace Equation.Solver;
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.Intrinsics;
+using System.Runtime.Intrinsics.X86;
+
+namespace Equation.Solver;
 
 internal readonly struct NandOperator
 {
@@ -9,5 +13,20 @@ internal readonly struct NandOperator
     {
         _leftValueIndex = leftValueIndex;
         _rightValueIndex = rightValueIndex;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Vector256<int> Nand(Vector256<int> left, Vector256<int> right)
+    {
+        // C# does not yet optimize ~(x & y) into Avx2.AndNot(x, y)
+        // so here it's done manually instead
+        if (Avx2.IsSupported)
+        {
+            return Avx2.AndNot(left, right);
+        }
+        else
+        {
+            return ~(left & right);
+        }
     }
 }
--- Equation.Solver/ProblemEquation.cs
+++ Equation.Solver/ProblemEquation.cs
@@ -1,6 +1,4 @@
-﻿using System.Runtime.CompilerServices;
-using System.Runtime.Intrinsics;
-using System.Runtime.Intrinsics.X86;
+﻿using System.Runtime.Intrinsics;
 
 namespace Equation.Solver;
 
@@ -22,48 +20,36 @@ internal sealed class ProblemEquation
         _outputSize = outputSize;
     }
 
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public unsafe ReadOnlySpan<Vector256<int>> Calculate(EquationValues equationValues)
     {
         Vector256<int>* results = equationValues.OperatorResults;
         Vector256<int>* allValues = equationValues.AllValues;
-        if (Avx2.IsSupported)
+
+        // 4x loop unrolled, no bounds checks,
+        // version of the code found in the else case
+        fixed (NandOperator* operators = _nandOperators)
         {
-            // Avx2 optimized, 4x loop unrolled, no bounds checks,
-            // version of the code found in the else case
-            fixed (NandOperator* operators = _nandOperators)
+            int* indexes = ConvertNandArrayToIntArray(operators);
+            for (int i = 0; i < _nandOperators.Length; i += _unrollFactor)
             {
-                int* indexes = ConvertNandArrayToIntArray(operators);
-                for (int i = 0; i < _nandOperators.Length; i += _unrollFactor)
-                {
-                    var op1Left = Avx.LoadAlignedVector256((int*)(allValues + (*(indexes + 0))));
-                    var op1Right = Avx.LoadAlignedVector256((int*)(allValues + (*(indexes + 1))));
-                    var op2Left = Avx.LoadAlignedVector256((int*)(allValues + (*(indexes + 2))));
-                    var op2Right = Avx.LoadAlignedVector256((int*)(allValues + (*(indexes + 3))));
-                    var op3Left = Avx.LoadAlignedVector256((int*)(allValues + (*(indexes + 4))));
-                    var op3Right = Avx.LoadAlignedVector256((int*)(allValues + (*(indexes + 5))));
-                    var op4Left = Avx.LoadAlignedVector256((int*)(allValues + (*(indexes + 6))));
-                    var op4Right = Avx.LoadAlignedVector256((int*)(allValues + (*(indexes + 7))));
-                    var result1 = Avx2.AndNot(op1Left, op1Right);
-                    var result2 = Avx2.AndNot(op2Left, op2Right);
-                    var result3 = Avx2.AndNot(op3Left, op3Right);
-                    var result4 = Avx2.AndNot(op4Left, op4Right);
-                    Avx.StoreAligned((int*)(results + 0), result1);
-                    Avx.StoreAligned((int*)(results + 1), result2);
-                    Avx.StoreAligned((int*)(results + 2), result3);
-                    Avx.StoreAligned((int*)(results + 3), result4);
-                    indexes += _unrollFactor * 2;
-                    results += _unrollFactor;
-                }
-            }
-        }
-        else
-        {
-            for (int i = 0; i < _nandOperators.Length; i++)
-            {
-                var leftValue = allValues[_nandOperators[i]._leftValueIndex];
-                var rightValue = allValues[_nandOperators[i]._rightValueIndex];
-                results[i] = ~(leftValue & rightValue);
+                var op1Left = Vector256.LoadAligned((int*)(allValues + (*(indexes + 0))));
+                var op1Right = Vector256.LoadAligned((int*)(allValues + (*(indexes + 1))));
+                var op2Left = Vector256.LoadAligned((int*)(allValues + (*(indexes + 2))));
+                var op2Right = Vector256.LoadAligned((int*)(allValues + (*(indexes + 3))));
+                var op3Left = Vector256.LoadAligned((int*)(allValues + (*(indexes + 4))));
+                var op3Right = Vector256.LoadAligned((int*)(allValues + (*(indexes + 5))));
+                var op4Left = Vector256.LoadAligned((int*)(allValues + (*(indexes + 6))));
+                var op4Right = Vector256.LoadAligned((int*)(allValues + (*(indexes + 7))));
+                var result1 = NandOperator.Nand(op1Left, op1Right);
+                var result2 = NandOperator.Nand(op2Left, op2Right);
+                var result3 = NandOperator.Nand(op3Left, op3Right);
+                var result4 = NandOperator.Nand(op4Left, op4Right);
+                result1.StoreAligned((int*)(results + 0));
+                result2.StoreAligned((int*)(results + 1));
+                result3.StoreAligned((int*)(results + 2));
+                result4.StoreAligned((int*)(results + 3));
+                indexes += _unrollFactor * 2;
+                results += _unrollFactor;
             }
         }
 
