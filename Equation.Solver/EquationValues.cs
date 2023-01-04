bb2da93fa62ab6ef3dc1586736a7b27dcb4460a3
TheAIBot <andreasuranus@hotmail.com>
2023-01-04 23:47:36 +0100

    Remove const values since they do not do anything useful

--- Equation.Solver/EquationValues.cs
+++ Equation.Solver/EquationValues.cs
@@ -5,24 +5,21 @@ namespace Equation.Solver;
 
 internal unsafe sealed class EquationValues
 {
-    const int constantsCount = 2;
     public readonly Vector256<int>* AllValues;
     public readonly int _size;
     public readonly Vector256<int>* OperatorResults;
     private readonly int _parameterCount;
 
-    public int StaticResultSize => constantsCount + _parameterCount;
+    public int StaticResultSize => _parameterCount;
 
     public EquationValues(int parameterCount, int operatorCount)
     {
-        _size = constantsCount + parameterCount + operatorCount;
+        _size = parameterCount + operatorCount;
         _parameterCount = parameterCount;
         // For vectorized code, aligned load/stores can be important in order to achieve optimal performance.
         // That's why we align this array by the vectors size so only aligned loads/stores are done.
         AllValues = (Vector256<int>*)NativeMemory.AlignedAlloc((nuint)(sizeof(Vector256<int>) * _size), (nuint)sizeof(Vector256<int>));
-        AllValues[0] = Vector256<int>.Zero;
-        AllValues[1] = Vector256<int>.AllBitsSet;
-        OperatorResults = AllValues + constantsCount + parameterCount;
+        OperatorResults = AllValues + parameterCount;
     }
 
     public void SetParameters(ProblemInput parameters)
@@ -31,7 +28,7 @@ internal unsafe sealed class EquationValues
         {
             throw new ArgumentOutOfRangeException(nameof(parameters));
         }
-        var to = new Span<Vector256<int>>(AllValues + constantsCount, _parameterCount);
+        var to = new Span<Vector256<int>>(AllValues, _parameterCount);
         parameters.Inputs.CopyTo(to);
     }
 }
