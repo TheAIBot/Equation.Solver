6c7c54432cc1c0c3282d670cb60039e7fca4fae3
TheAIBot <andreasuranus@hotmail.com>
2025-12-23 22:04:39 +0100

    Optimize indexing

--- Equation.Solver/NandOperator.cs
+++ Equation.Solver/NandOperator.cs
@@ -5,23 +5,23 @@ namespace Equation.Solver;
 
 internal readonly struct NandOperator
 {
-    private readonly int _leftValueIndex;
-    private readonly int _rightValueIndex;
+    private readonly uint _leftValueIndex;
+    private readonly uint _rightValueIndex;
 
-    public int LeftValueIndex => _leftValueIndex / Vector256<int>.Count;
-    public int RightValueIndex => _rightValueIndex / Vector256<int>.Count;
+    public readonly int LeftValueIndex => (int)(_leftValueIndex / (uint)Vector256<int>.Count);
+    public readonly int RightValueIndex => (int)(_rightValueIndex / (uint)Vector256<int>.Count);
 
     public NandOperator(int leftValueIndex, int rightValueIndex)
     {
         // Pre-multiplied by 8 since these are not an index into
         // a Vector256<int>[] but an int[]  where 8 elements
         // together are an Vector256<int>.
-        _leftValueIndex = leftValueIndex * Vector256<int>.Count;
-        _rightValueIndex = rightValueIndex * Vector256<int>.Count;
+        _leftValueIndex = (uint)(leftValueIndex * Vector256<int>.Count);
+        _rightValueIndex = (uint)(rightValueIndex * Vector256<int>.Count);
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public unsafe Vector256<int> Nand(int* allValues, int* inputs, int inputCount)
+    public readonly unsafe Vector256<int> Nand(int* allValues, int* inputs, int inputCount)
     {
         int* leftInput = _leftValueIndex < inputCount * Vector256<int>.Count ? inputs : allValues;
         Vector256<int> opLeft = Vector256.LoadAligned(leftInput + _leftValueIndex);
