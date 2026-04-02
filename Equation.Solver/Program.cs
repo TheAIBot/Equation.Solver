103ed4efe846fdbe81080e39940cced6874f39bb
TheAIBot <andreasuranus@hotmail.com>
2026-04-02 02:34:32 +0200

    SImple way to generate text math input and text math output and solve on it.

--- Equation.Solver/Program.cs
+++ Equation.Solver/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Equation.Solver.Solvers;
+using System.Text;
 
 namespace Equation.Solver;
 
@@ -6,13 +7,19 @@ internal sealed class Program
 {
     static async Task Main(string[] args)
     {
-        //ProblemExample[] examples = ProblemExample.ConvertToExamples().ToArray();
+        //IEnumerable<(bool[] inputs, bool[] outputs)> examples = CreateBiArgOperatorExamplesAsInts(1_000, 10, (x, y) => x + y);
+        //ProblemExample[] examples = ProblemExample.ConvertToExamples(examples).ToArray();
+
+        IEnumerable<(bool[] inputs, bool[] outputs)> examples = NormalizeExampleSizes(200, 100, CreateTextMathExamples(10_000, -10_000, 10_000));
+
+        long totalOutputBits = examples.Sum(x => x.outputs.LongLength);
+        Console.WriteLine($"Total output bits: {totalOutputBits:N0}");
 
         IExampleCluster exampleCluster = new RandomExampleCluster([
             new RandomExampleClustering(1.0f, 10),
             new RandomExampleClustering(0.3f, 10),
         ]);
-        var exampleClusters = exampleCluster.ToClusters(CreateBiArgOperatorExamplesAsInts(1_000, 10, (x, y) => x + y));
+        var exampleClusters = exampleCluster.ToClusters(examples);
         IEnumerable<ProblemExample[]> problemClusters = exampleClusters.Select(x => ProblemExample.ConvertToExamples(x).ToArray());
 
         var problems = problemClusters.Select(x => new EquationProblem(x)).ToArray();
@@ -21,8 +28,8 @@ internal sealed class Program
         //ISolver solver = new ParallelSolver(new RandomEvolutionSolver(problem.ParameterCount, 1000, 100_000, 0.1f, 0.0025f, 0.0001f, 0.5f));
         //ISolver solver = new ParallelSolver(new RandomEvolutionSolverWithEquationCombining(problem.ParameterCount, 1000, problem.OutputCount, 100_000, 0.01f, 0.0025f, 0.001f, 0.001f, 0.5f));
         //ISolver solver = new RandomChunkEvolutionSolver(100, 10_000, new RandomChunkEvolver(200, 10_000, 0.1f, 0.02f, problem.ParameterCount, problem.OutputCount));
-        int operatorCount = 1_000;
-        ISolver solver = new ParallelMixSolver(new RandomEvolutionSolverWithEquationCombining(problems[0].ParameterCount, operatorCount, problems[0].OutputCount, 10_00, 0.01f, 0.0025f, 0.01f, 0.001f, 0.1f),
+        int operatorCount = 10_000;
+        ISolver solver = new ParallelMixSolver(new RandomEvolutionSolverWithEquationCombining(problems[0].ParameterCount, operatorCount, problems[0].OutputCount, 10_000, 0.01f, 0.0025f, 0.01f, 0.001f, 0.1f),
                                                problems,
                                                problems[0],
                                                operatorCount,
@@ -101,5 +108,63 @@ internal sealed class Program
 
             yield return (inputs, outputs);
         }
+    }
+
+    private static IEnumerable<(bool[] inputs, bool[] outputs)> NormalizeExampleSizes(int inputSize, int outputSize, IEnumerable<(bool[] inputs, bool[] outputs)> examples)
+    {
+        foreach (var example in examples)
+        {
+            var input = example.inputs;
+            Array.Resize(ref input, inputSize);
+            var output = example.outputs;
+            Array.Resize(ref output, outputSize);
+            yield return (input, output);
+        }
+    }
+
+    private static IEnumerable<(bool[] inputs, bool[] outputs)> CreateTextMathExamples(int exampleCountPerBiArgSymbol,
+                                                                                       int minIncusiveNumber,
+                                                                                       int maxExclusiveNumber)
+    {
+        var random = new Random(94);
+        var add = CreateTextMathExamplesForBiArgSymbol(random, exampleCountPerBiArgSymbol, minIncusiveNumber, maxExclusiveNumber, (a, b) => a + b, "+");
+        var subtract = CreateTextMathExamplesForBiArgSymbol(random, exampleCountPerBiArgSymbol, minIncusiveNumber, maxExclusiveNumber, (a, b) => a - b, "-");
+
+        return add.Concat(subtract);
+    }
+
+    private static IEnumerable<(bool[] inputs, bool[] outputs)> CreateTextMathExamplesForBiArgSymbol(Random random,
+                                                                                                     int exampleCount,
+                                                                                                     int minIncusiveNumber,
+                                                                                                     int maxExclusiveNumber,
+                                                                                                     Func<int, int, int> operation,
+                                                                                                     string biArgOperationSymbol)
+    {
+        for (int exampleCounter = 0; exampleCounter < exampleCount; exampleCounter++)
+        {
+            int a = random.Next(minIncusiveNumber, maxExclusiveNumber);
+            int b = random.Next(minIncusiveNumber, maxExclusiveNumber);
+            int result = operation(a, b);
+            string input = $"{a} {biArgOperationSymbol} {b} =";
+            string output = $"{result}";
+
+            yield return (TextToBools(input), TextToBools(output));
+        }
+    }
+
+    private static bool[] TextToBools(string text)
+    {
+        byte[] utf8Bytes = Encoding.UTF8.GetBytes(text);
+        List<bool> bools = [];
+        for (int byteIndex = 0; byteIndex < utf8Bytes.Length; byteIndex++)
+        {
+            const int bitsPerByte = 8;
+            for (int bitIndex = 0; bitIndex < bitsPerByte; bitIndex++)
+            {
+                bools.Add(((utf8Bytes[byteIndex] >> bitIndex) & 1) == 1);
+            }
+        }
+
+        return bools.ToArray();
     }
 }
