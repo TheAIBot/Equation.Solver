e238337b39b7b83a5864492d62404d8a433844b1
TheAIBot <andreasuranus@hotmail.com>
2026-04-04 22:38:56 +0200

    Make new nand changer that only changes already use

---
+++ Equation.Solver/Evolvers/NandChangerOnlyUsedOperators.cs
@@ -0,0 +1,35 @@
+﻿namespace Equation.Solver.Evolvers;
+
+internal sealed class NandChangerOnlyUsedOperators
+{
+    private readonly List<int> _usedIndexes = [];
+
+    /// <summary>
+    /// Randomly changes the inputs of randomly selected operators.
+    /// </summary>
+    /// <returns>False if change has no effect on output and True if it may have an effect.</returns>
+    public bool RandomizeSmallPartOfEquation(Random random, ProblemEquation equation, EquationValues equationValues, int operatorCountToRandomize)
+    {
+        _usedIndexes.Clear();
+        for (int i = 0; i < equation.OperatorsUsed.Length; i++)
+        {
+            if (equation.OperatorsUsed[i])
+            {
+                _usedIndexes.Add(i);
+            }
+        }
+
+        Span<NandOperator> operators = equation.NandOperators;
+        int inputParameterCount = equationValues.InputParameterCount;
+        for (int i = 0; i < operatorCountToRandomize; i++)
+        {
+            int operatorIndex = _usedIndexes[random.Next(0, _usedIndexes.Count)];
+            int leftValueIndex = random.Next(0, inputParameterCount + operatorIndex);
+            int rightValueIndex = random.Next(0, inputParameterCount + operatorIndex);
+            operators[operatorIndex] = new NandOperator(leftValueIndex, rightValueIndex);
+        }
+
+        equation.RecalculateOperatorsUsed(inputParameterCount);
+        return true;
+    }
+}
