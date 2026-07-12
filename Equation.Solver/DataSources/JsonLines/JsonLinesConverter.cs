c05498a561108dc67b12e0167ef18550410a128c
TheAIBot <andreasuranus@hotmail.com>
2026-07-12 21:44:32 +0200

    Parallelize reading and transforming json lines

--- Equation.Solver/DataSources/JsonLines/JsonLinesConverter.cs
+++ Equation.Solver/DataSources/JsonLines/JsonLinesConverter.cs
@@ -1,48 +1,25 @@
 ﻿using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization.Metadata;
-using System.Threading.Channels;
 
 namespace Equation.Solver.DataSources.JsonLines;
 
 internal sealed class JsonLinesConverter
 {
-    public static async IAsyncEnumerable<T> ReadJsonLines<T>(string filePath)
+    public static IAsyncEnumerable<T> ReadJsonLines<T>(string filePath)
         where T : IJsonConverterType<T>
     {
         JsonTypeInfo<T> jsonTypeInfo = T.GetJsonTypeInfo();
-        await foreach (string line in File.ReadLinesAsync(filePath))
-        {
-            yield return JsonSerializer.Deserialize(line, jsonTypeInfo) ?? throw new InvalidOperationException($"Failed to deserialize json: \"{line}\"");
-        }
+        return Parallel.Transform(File.ReadLines(filePath),
+                                  line => JsonSerializer.Deserialize(line, jsonTypeInfo) ?? throw new InvalidOperationException($"Failed to deserialize json: \"{line}\""));
     }
 
-    public static async IAsyncEnumerable<TOutput> ReadJsonLines<TInput, TOutput>(string inputFilePath, Func<TInput, TOutput> converter)
+    public static IAsyncEnumerable<TOutput> ReadJsonLines<TInput, TOutput>(string inputFilePath, Func<TInput, TOutput> converter)
         where TInput : IJsonConverterType<TInput>
         where TOutput : IJsonConverterType<TOutput>
     {
-        Channel<TOutput> outputs = Channel.CreateUnbounded<TOutput>(new UnboundedChannelOptions() { SingleWriter = true });
-        Task readingData = Task.Run(async () =>
-        {
-            try
-            {
-                await foreach (TInput input in ReadJsonLines<TInput>(inputFilePath))
-                {
-                    await outputs.Writer.WriteAsync(converter(input));
-                }
-            }
-            finally
-            {
-                outputs.Writer.Complete();
-            }
-        });
-
-        await foreach (var item in outputs.Reader.ReadAllAsync())
-        {
-            yield return item;
-        }
-
-        await readingData;
+        return Parallel.Transform(ReadJsonLines<TInput>(inputFilePath),
+                                  converter);
     }
 
 
---
+++ Equation.Solver/Parallel.cs
@@ -0,0 +1,86 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks.Dataflow;
+
+namespace Equation.Solver;
+
+internal static class ParallelExtensions
+{
+    extension(Parallel)
+    {
+        public static IAsyncEnumerable<TTo> Transform<TFrom, TTo>(IEnumerable<TFrom> from, Func<TFrom, TTo> transform)
+        {
+            return Transform(from.ToAsyncEnumerable(), x => ValueTask.FromResult(transform(x)));
+        }
+
+        public static IAsyncEnumerable<TTo> Transform<TFrom, TTo>(IAsyncEnumerable<TFrom> from, Func<TFrom, TTo> transform)
+        {
+            return Transform(from, x => ValueTask.FromResult(transform(x)));
+        }
+
+        public static IAsyncEnumerable<TTo> Transform<TFrom, TTo>(IEnumerable<TFrom> from, Func<TFrom, ValueTask<TTo>> transform)
+        {
+            return Transform(from.ToAsyncEnumerable(), transform);
+        }
+
+        public static async IAsyncEnumerable<TTo> Transform<TFrom, TTo>(IAsyncEnumerable<TFrom> from, Func<TFrom, ValueTask<TTo>> transform)
+        {
+            var transformer = new TransformBlock<TFrom, ValueOrException<TTo>>(
+                async x =>
+                {
+                    try
+                    {
+                        return new ValueOrException<TTo>(await transform(x), null);
+                    }
+                    catch (Exception e)
+                    {
+                        return new ValueOrException<TTo>(default, e);
+                    }
+                },
+                new ExecutionDataflowBlockOptions()
+                {
+                    MaxDegreeOfParallelism = Environment.ProcessorCount - 1,
+                    SingleProducerConstrained = true,
+                    EnsureOrdered = true
+                });
+
+            Task addValues = Task.Run(async () =>
+            {
+                try
+                {
+                    await foreach (var value in from)
+                    {
+                        if (!await transformer.SendAsync(value))
+                        {
+                            throw new InvalidOperationException("Failed to enqueue element.");
+                        }
+                    }
+                }
+                finally
+                {
+                    transformer.Complete();
+                }
+            });
+
+            await foreach (var result in transformer.ReceiveAllAsync())
+            {
+                if (result.HasException)
+                {
+                    throw result.Exception;
+                }
+
+                yield return result.Value;
+            }
+
+            await addValues;
+        }
+    }
+
+
+
+    private readonly record struct ValueOrException<T>(T? Value, Exception? Exception)
+    {
+        [MemberNotNullWhen(true, nameof(Exception))]
+        [MemberNotNullWhen(false, nameof(Value))]
+        public bool HasException => Exception != null;
+    }
+}
